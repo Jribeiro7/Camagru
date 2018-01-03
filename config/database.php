c2260e42b44054eeece075b8c2c02d1b4568999e
Jimmy RIBEIRO <jribeiro@e1r11p17.42.fr>
2018-01-03 20:08:31 +0100

    Setup init finish

--- config/database.php
+++ config/database.php
@@ -1,5 +1,5 @@
 <?PHP
-	$DB_DSN = 'mysql:host=localhost;dbname=camagru';
-	$DB_NAME = 'root';
-	$DB_PASSWORD = '';
+	$DB_DSN = "mysql:host=127.0.0.1";
+	$DB_USER = "root";
+	$DB_PASSWORD = "xvlda";
 ?>
--- config/setup.php
+++ config/setup.php
@@ -1,12 +1,33 @@
 <?PHP
 
-require_once('./database.php');
+require_once("database.php");
 
 try {
 		$db = new PDO($DB_DSN, $DB_USER, $DB_PASSWORD);
 		$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-		$db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
-		$db->exec(file_get_contents('camagru.sql'));
+		$db->exec('CREATE DATABASE IF NOT EXISTS `camagru` DEFAULT
+					CHARACTER SET utf8 COLLATE utf8_general_ci');
+		$sql = 'USE `camagru`;
+				CREATE TABLE IF NOT EXISTS `users` (
+					`id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+					`login` varchar(25) NOT NULL,
+					`mail` varchar(255) NOT NULL,
+					`password` varchar(255),
+					`token` varchar(255)
+				);
+				CREATE TABLE IF NOT EXISTS `images` (
+					`id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+					`id_user` INT DEFAULT NULL,
+					`path` varchar(255) DEFAULT NULL,
+					`likes` INT DEFAULT NULL
+				);
+				CREATE TABLE IF NOT EXISTS `comments` (
+					`id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+					`id_user` INT DEFAULT NULL,
+					`id_image` INT DEFAULT NULL,
+					`comment` varchar(255) DEFAULT NULL
+				);';
+		$db->exec($sql);
 		echo "Setup finish <br />";
 } catch (PDOException $e) {
 		echo "Setup error : " . $e->getMessage();
